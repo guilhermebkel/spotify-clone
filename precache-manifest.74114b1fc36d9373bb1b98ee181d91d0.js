723ee3bcd9febe2f7b08b98850364fe8edf9f5f5
Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-05 08:39:25 -0300

    Updates

---
+++ precache-manifest.74114b1fc36d9373bb1b98ee181d91d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08834fc4fc51b4ed6de65c8c65708273",
+    "url": "/Fake-Spotify/index.html"
+  },
+  {
+    "revision": "7a1765946d6aa66c18be",
+    "url": "/Fake-Spotify/static/css/main.032c8bb6.chunk.css"
+  },
+  {
+    "revision": "c89ca81dd4c396552f89",
+    "url": "/Fake-Spotify/static/js/2.307756a9.chunk.js"
+  },
+  {
+    "revision": "7a1765946d6aa66c18be",
+    "url": "/Fake-Spotify/static/js/main.0b3cbc01.chunk.js"
+  },
+  {
+    "revision": "e44341421727603da8d4",
+    "url": "/Fake-Spotify/static/js/runtime~main.c39fbf9c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://localhost:3000/precache-manifest.2939507433bb4bf886744c06ac27eb3c.js"
+  "/Fake-Spotify/precache-manifest.74114b1fc36d9373bb1b98ee181d91d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://localhost:3000/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fake-Spotify/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
