211bcba388f0882a5c7443742f4e67bb45b55940
Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-05 22:22:55 -0300

    Updates

---
+++ precache-manifest.7ab57115249d96585eba622bede0f887.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448879bb18048190ac47358637434197",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "7f08619bed66febc1c4b",
+    "url": "http://localhost:3000/static/css/main.442ffa4f.chunk.css"
+  },
+  {
+    "revision": "5f5d00b12372d39b641a",
+    "url": "http://localhost:3000/static/js/2.d7607455.chunk.js"
+  },
+  {
+    "revision": "7f08619bed66febc1c4b",
+    "url": "http://localhost:3000/static/js/main.8249d08e.chunk.js"
+  },
+  {
+    "revision": "e8d955eac6f650306bb5",
+    "url": "http://localhost:3000/static/js/runtime~main.8a6cbfe7.js"
+  }
+]);
