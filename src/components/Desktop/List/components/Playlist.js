bda32d8f2f4165150b8e40e54d92991de988f4c9
Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-07 15:20:53 -0300

    Added method to go to profile

--- src/components/Desktop/List/components/Playlist.js
+++ src/components/Desktop/List/components/Playlist.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-const Playlist = ({ state }) => (
-    <div className="playlist">
-        <img alt="cover" className="cover" src={state.playlist.cover_url} />
-        <section className="playlist-info">
-            <h2 className="subtitle">PLAYLIST</h2>
-            <h1 className="title">{state.playlist.name}</h1>
-            <h3 className="author">Created by <strong>{state.playlist.created_by}</strong> • {state.playlist.total_songs} songs</h3>
-            <button className="play-button">PLAY</button>
-        </section>
-    </div>
-)
+class Playlist extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    profile = () => {
+
+        const data = {
+            selector: '',
+            type: 'profile'
+        }
+    
+        const getProfile = (data) => ({ type: 'GET_PROFILE', data })
+        this.props.dispatch(getProfile(data))
+    }
+
+    render(){
+        return(
+            <div className="playlist">
+                <img alt="cover" className="cover" src={this.props.state.playlist.cover_url} />
+                <section className="playlist-info">
+                    <h2 className="subtitle">PLAYLIST</h2>
+                    <h1 className="title">{this.props.state.playlist.name}</h1>
+                    <h3 className="author">Created by <strong onClick={() => this.profile()}>{this.props.state.playlist.created_by}</strong> • {this.props.state.playlist.total_songs} songs</h3>
+                    <button className="play-button">PLAY</button>
+                </section>
+            </div>
+        )
+    }
+}
 
 export default connect(state => ({ state }))(Playlist)
