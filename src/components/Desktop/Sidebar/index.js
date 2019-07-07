8c4169f7e90a79c73dcfbce0011799c0f74426ef
Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-07 11:26:08 -0300

    Added method to change style on sidebar selector

--- src/components/Desktop/Sidebar/index.js
+++ src/components/Desktop/Sidebar/index.js
@@ -6,20 +6,24 @@ import Home from '../../../assets/icons/home-black-shape.png'
 
 const Sidebar = ({ state }) => (
     <div className="sidebar">
-        <div className="selector">
+        <div key="home" id={state.selector === 'home' ? 'selected' : ''} className="selector">
             <img alt="home" src={Home} />
             <h1>Home</h1>
         </div>
         <h1 className="library-title">YOUR LIBRARY</h1>
         <li className="library-list">
-            <ul key="made-for-you">Made for you</ul>
-            <ul key="recently-played">Recently played</ul>
-            <ul key="songs" id="selected">Songs</ul>
-            <ul key="albums">Albums</ul>
-            <ul key="artists">Artists</ul>
+            <ul key="made-for-you" id={state.selector === 'made-for-you' ? 'selected' : ''}>Made for you</ul>
+            <ul key="recently-played" id={state.selector === 'recently-played' ? 'selected' : ''}>Recently played</ul>
+            <ul key="songs" id={state.selector === 'songs' ? 'selected' : ''}>Songs</ul>
+            <ul key="albums" id={state.selector === 'albums' ? 'selected' : ''}>Albums</ul>
+            <ul key="artists" id={state.selector === 'artists' ? 'selected' : ''}>Artists</ul>
         </li>
         <h1 className="playlist-title">PLAYLISTS</h1>
-        {state.playlists.map(playlist => <ul key={playlist.name} id={playlist.name} className="playlist-name">{playlist.name}</ul>)}
+            {state.playlists.map(playlist => 
+                <ul key={playlist.name} id={state.selector === playlist.nome ? 'selected' : ''} className="playlist-name">
+                    {playlist.name}
+                </ul>
+            )}
     </div>
 )
 
