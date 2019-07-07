49d74c5fd4cf9e1289cab17d44db1416d28c328b
Guilherme <guilhermebromonschenkel@gmail.com>
2019-07-07 17:27:56 -0300

    Added new profile info

--- src/components/Desktop/Profile/index.js
+++ src/components/Desktop/Profile/index.js
@@ -4,13 +4,21 @@ import { connect } from 'react-redux'
 import './style.css'
 
 const Profile = ({ state }) => (
-    <div className="profile">
-        <img alt="cover" className="cover" src={state.avatar_url} />
-        <section className="user-info">
-            <h2 className="subtitle">USER</h2>
-            <h1 className="title">{state.name}</h1>
-        </section>
-    </div>
+    <>
+        <div className="profile">
+            <img alt="cover" className="cover" src={state.avatar_url} />
+            <section className="user-info">
+                <h2 className="subtitle">USER</h2>
+                <h1 className="title">{state.name}</h1>
+            </section>
+        </div>
+        <li className="profile-options">
+            <ul className="selected">OVERVIEW</ul>
+            <ul>PUBLIC PLAYLISTS</ul>
+            <ul>FOLLOWING</ul>
+            <ul>FOLLOWERS({state.followers})</ul>
+        </li>
+    </>
 )
 
 export default connect(state => ({ state }))(Profile)
